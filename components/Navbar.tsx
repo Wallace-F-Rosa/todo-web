2b230c526497bb44f1fcfb09bb5d56b2f06aa5de
Wallace Ferancini Rosa <wferancini@gmail.com>
2023-10-23 19:31:43 -0300

    feature(Navbar): added avatar with dropdown menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,26 +2,25 @@
 import Image from 'next/image'
 import TodoLogo from '@/public/todo.svg'
 import React from 'react'
-import { Button } from 'flowbite-react'
-import Link from 'next/link'
+import { Avatar, Dropdown } from 'flowbite-react'
 
 export default function Navbar() {
     return (
         <nav className='h-15 flex w-full flex-row border-b-2 border-gray-400 bg-primary-dark px-4 py-4'>
             <Image src={TodoLogo} alt='// TODO'></Image>
-            <div className='flex w-full justify-end justify-items-center gap-x-4'>
-                <Link
-                    href={'/login'}
-                    className='text-white hover:text-secondary-dark'
+            <div className='flex w-full justify-end justify-items-center gap-x-4 text-white'>
+                <Dropdown
+                    arrowIcon={false}
+                    inline
+                    label={
+                        <Avatar rounded className='flex-row-reverse gap-x-4'>
+                            <div className='font-medium'>Username</div>
+                        </Avatar>
+                    }
                 >
-                    Log in
-                </Link>
-                <Link
-                    href={'/register'}
-                    className='text-white hover:text-secondary-dark'
-                >
-                    Register
-                </Link>
+                    <Dropdown.Item>My info</Dropdown.Item>
+                    <Dropdown.Item>About this project</Dropdown.Item>
+                </Dropdown>
             </div>
         </nav>
     )
