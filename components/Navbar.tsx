c9bf003a35f32d3349b35a3b7ff397ae7a886a24
Wallace Ferancini Rosa <wferancini@gmail.com>
2023-10-31 21:45:37 -0300

    feature(Navbar): variant for login or logged state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image'
 import TodoLogo from '@/public/todo.svg'
 import React from 'react'
 import { Avatar, Dropdown } from 'flowbite-react'
+import Link from 'next/link'
 
-export default function Navbar() {
-    return (
-        <nav className='h-15 flex w-full flex-row border-b-2 border-gray-400 bg-primary-dark px-4 py-4'>
-            <Image src={TodoLogo} alt='// TODO'></Image>
+export default function Navbar({ variant }) {
+    let navComplement = null
+    if (variant == 'logged') {
+        navComplement = (
             <div className='flex w-full justify-end justify-items-center gap-x-4 text-white'>
                 <Dropdown
                     arrowIcon={false}
@@ -22,6 +23,13 @@ export default function Navbar() {
                     <Dropdown.Item>About this project</Dropdown.Item>
                 </Dropdown>
             </div>
+        )
+    }
+
+    return (
+        <nav className='h-15 flex w-full flex-row border-b-2 border-gray-400 bg-primary-dark px-4 py-4'>
+            <Image src={TodoLogo} alt='// TODO'></Image>
+            {navComplement != null ? navComplement : <></>}
         </nav>
     )
 }
